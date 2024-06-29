6aae98a371a5cca5a19a8991647d840921a440fd
ast404 <aleks.hobby.404@gmail.com>
2024-06-29 21:41:14 +0200

    Add unit tests for the Tournament.

--- src/tournament.rs
+++ src/tournament.rs
@@ -39,3 +39,101 @@ impl Tournament {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::player::GamePlay;
+    use crate::game::Move;
+    use crate::strategy::Strategy;
+
+    struct MockStrategy {
+        next_move: Move,
+    }
+
+    impl Strategy for MockStrategy {
+        fn play(&self, _past_games: &[GamePlay]) -> Move {
+            self.next_move
+        }
+    }
+
+    #[test]
+    fn all_pairs_single_player() {
+        let mock_strategy = MockStrategy {
+            next_move: Move::Defect,
+        };
+        let tournament = Tournament::all_pairs();
+        let mut players = vec![Player::new("single_player", &mock_strategy)];
+        tournament.play_games(& mut players);
+        assert_eq!(players[0].score(), 0);
+    }
+
+    #[test]
+    fn all_pairs_two_players() {
+        let collaborate_strategy = MockStrategy {
+            next_move: Move::Collaborate,
+        };
+        let defect_strategy = MockStrategy {
+            next_move: Move::Defect,
+        };
+        let tournament = Tournament::all_pairs();
+        let mut players = vec![
+            Player::new("collaborate_player", &collaborate_strategy),
+            Player::new("defect_player", &defect_strategy),
+        ];
+        tournament.play_games(& mut players);
+        assert_eq!(players[0].score(), 0);
+        assert_eq!(players[1].score(), 50);
+    }
+
+    #[test]
+    fn all_pairs_three_players() {
+        let collaborate_strategy = MockStrategy {
+            next_move: Move::Collaborate,
+        };
+        let defect_strategy = MockStrategy {
+            next_move: Move::Defect,
+        };
+        let tournament = Tournament::all_pairs();
+        let mut players = vec![
+            Player::new("collaborate_player", &collaborate_strategy),
+            Player::new("defect_player", &defect_strategy),
+            Player::new("defect_player_2", &defect_strategy),
+        ];
+        tournament.play_games(& mut players);
+        assert_eq!(players[0].score(), 0);
+        assert_eq!(players[1].score(), 60);
+        assert_eq!(players[2].score(), 60);
+    }
+
+    #[test]
+    fn axelrod_single_player() {
+        let mock_strategy = MockStrategy {
+            next_move: Move::Defect,
+        };
+        let tournament = Tournament::axelrod_tournament();
+        let mut players = vec![Player::new("single_player", &mock_strategy)];
+        tournament.play_games(& mut players);
+        assert_eq!(players[0].score(), 10);
+    }
+
+    #[test]
+    fn axelrod_three_players() {
+        let collaborate_strategy = MockStrategy {
+            next_move: Move::Collaborate,
+        };
+        let defect_strategy = MockStrategy {
+            next_move: Move::Defect,
+        };
+        let tournament = Tournament::axelrod_tournament();
+        let mut players = vec![
+            Player::new("collaborate_player", &collaborate_strategy),
+            Player::new("defect_player", &defect_strategy),
+            Player::new("defect_player_2", &defect_strategy),
+        ];
+        tournament.play_games(& mut players);
+        assert_eq!(players[0].score(), 30);
+        assert_eq!(players[1].score(), 70);
+        assert_eq!(players[2].score(), 70);
+    }
+}
